43317b4b551114a9c132287760f4659aaefe42c8
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-08-15 20:32:57 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+board = [" X ","   ","   ","   ","   ","   ","   ","   ","   "]
 
 def display_board(board)
   puts "#{board[1]}   ""|""#{board[2]}   ""|""#{board[3]}   "
